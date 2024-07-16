177dd3eb7bdb5d2e1b9ef700b1a3c8469f3de36d
matheusfilizzola <matheusfilizzola98@gmail.com>
2024-07-16 00:42:19 -0300

    ragdoll mechanic added

--- 67BitsTestUnity/Assets/Scripts/Enemies/EnemiesController.cs
+++ 67BitsTestUnity/Assets/Scripts/Enemies/EnemiesController.cs
@@ -1,22 +1,50 @@
 using UnityEngine;
 using MyUtilities;
+using System.Collections;
 
 public class EnemiesController : MonoBehaviour, Idamageble
 {
-    public Rigidbody rig;
-    private void Start()
-    {
-        rig = GetComponent<Rigidbody>();
+    [Header("Stacking Enemy Settings")]
+    [SerializeField] Stacking stackingManager;
+    [SerializeField] public RagdollEnabler[] Ragdolls;
+    public bool isAffected;
 
-    }
     public void TakeDamage(Transform targetTransform, int damage, float knockBackForce)
     {
         ApplyKnockBack(targetTransform, knockBackForce);
+        StartCoroutine(StackingEnemies());
     }
-    
 
     public void ApplyKnockBack(Transform targetTransform, float knockBackForce)
     {
-        rig.KnockBack(transform, targetTransform, knockBackForce);
+        foreach (RagdollEnabler ragdoll in Ragdolls)
+        {
+            ragdoll.EnableRagdoll();
+        }
+        foreach (RagdollEnabler ragdoll in Ragdolls)
+        {
+            foreach (Rigidbody rigidbody in ragdoll.rigidBodies)
+            {
+                rigidbody.KnockBack(transform, targetTransform, knockBackForce);
+            }
+        }
+    }
+
+    public IEnumerator StackingEnemies()
+    {
+        yield return new WaitForSeconds(1);
+
+        foreach (RagdollEnabler ragdoll in Ragdolls)
+        {
+            ragdoll.EnableAnimator();
+        }
+
+        gameObject.transform.parent = null;
+        stackingManager.cubes.Add(gameObject.transform);
+
+        if (stackingManager.cubes.Count > 1)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(-90, gameObject.transform.eulerAngles.y, 90);
+        }
     }
 }
